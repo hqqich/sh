4191681dfd230d99146a4c7e13aa13cf12fafd76
hqqich <hqqich1314@outlook.com>
2026-03-03 15:23:18 +0800

    提交代码

---
+++ README.md
@@ -0,0 +1,5 @@
+# 
+
+fork自：[kejilion/sh](https://github.com/kejilion/sh)
+
+从`科技lion`学习脚本开发
---
+++ init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sh_download() {
+    local script_path="$HOME/kejilion.sh"
+
+    # 检测国家/IPv6/IPv4
+    local country=$(curl -s --max-time 1 ipinfo.io/country || echo "unknown")
+    local ipv6_address=$(curl -s --max-time 1 https://v6.ipinfo.io/ip || echo "")
+    local ipv4_address=$(curl -s --max-time 1 https://ipinfo.io/ip || echo "")
+
+    if [ "$country" = "CN" ] || [ -n "$ipv6_address" ]; then
+        curl -sS -o "$script_path" "https://gh.kejilion.pro/raw.githubusercontent.com/hqqich/sh/main/base.sh"
+    else
+        curl -sS -o "$script_path" "https://raw.githubusercontent.com/hqqich/sh/main/base.sh"
+    fi
+
+    chmod +x "$script_path"
+    "$script_path" "$@"
+}
+
+sh_download "$@"
