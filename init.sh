036a542f0f5cfbad32362f118c0798b26dbbc2da
hqqich <hqqich1314@outlook.com>
2026-03-07 16:22:35 +0800

    完善脚本

--- init.sh
+++ init.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
 sh_download() {
-    local script_path="$HOME/sh/base.sh"
+    mkdir "$HOME/tsinglink-script"
+    local script_path="$HOME/tsinglink-script/base.sh"
+    local lan_url="http://172.22.90.8:5244/sh/base.sh"
 
-    # 检测国家/IPv6/IPv4
-    local country=$(curl -s --max-time 1 ipinfo.io/country || echo "unknown")
-    local ipv6_address=$(curl -s --max-time 1 https://v6.ipinfo.io/ip || echo "")
-    local ipv4_address=$(curl -s --max-time 1 https://ipinfo.io/ip || echo "")
-
-    if [ "$country" = "CN" ] || [ -n "$ipv6_address" ]; then
-        curl -sS -o "$script_path" "https://gh.kejilion.pro/raw.githubusercontent.com/hqqich/sh/main/base.sh"
+    # LAN 可达时优先使用
+    if curl -sS --head --max-time 1 "$lan_url" >/dev/null 2>&1; then
+        echo "内部下载"
+        curl -sS -o "$script_path" "$lan_url"
     else
-        curl -sS -o "$script_path" "https://raw.githubusercontent.com/hqqich/sh/main/base.sh"
+        echo "外部下载"
+        # 检测国家/IPv6/IPv4
+        local country=$(curl -s --max-time 1 ipinfo.io/country || echo "unknown")
+        local ipv6_address=$(curl -s --max-time 1 https://v6.ipinfo.io/ip || echo "")
+        local ipv4_address=$(curl -s --max-time 1 https://ipinfo.io/ip || echo "")
+
+        if [ "$country" = "CN" ] || [ -n "$ipv6_address" ]; then
+            curl -sS -o "$script_path" "https://gh.kejilion.pro/raw.githubusercontent.com/hqqich/sh/main/base.sh"
+        else
+            curl -sS -o "$script_path" "https://raw.githubusercontent.com/hqqich/sh/main/base.sh"
+        fi
     fi
 
     chmod +x "$script_path"
